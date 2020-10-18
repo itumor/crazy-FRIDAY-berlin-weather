7a46cc18f48fab80eb5694f31837ba49ba014ddf
ITUMOR <huma20050002hti28@gmail.com>
2020-10-18 22:53:16 +0300

    Update ouputs.tf

--- ouputs.tf
+++ ouputs.tf
@@ -1,6 +1,6 @@
 output "this_s3_bucket_id" {
   description = "The name of the bucket."
-  value       =  [module.s3_bucket.*.this_s3_bucket_id}
+  value       =  [module.s3_bucket.*.this_s3_bucket_id]
 }
 
 output "this_s3_bucket_arn" {
